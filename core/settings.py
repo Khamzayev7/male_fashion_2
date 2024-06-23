ab5846ee34c8e818e991ec31605c830004def32a
Nodirbek <xamzayevnodirbek29@gmail.com>
2024-06-23 22:04:47 +0500

    mesagge

--- core/settings.py
+++ core/settings.py
@@ -29,11 +29,11 @@ DJANGO_APPS = [
 ]
 
 CUSTOM_APPS = [
-
+    'shop',
 ]
 
 THIRD_PART_APPS = [
-
+    'ckeditor',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + CUSTOM_APPS + THIRD_PART_APPS
@@ -124,3 +124,16 @@ MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CKEDITOR_BASEPATH = "/my_static/ckeditor/ckeditor/"
+CKEDITOR_UPLOAD_PATH = "uploads/"
+
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': 'full',
+       # 'height': 300,
+       # 'width': 300,
+    },
+}
+
+AUTH_USER_MODEL = "user.UserModel"
---
+++ shop/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.db.models import Sum
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+from ckeditor_uploader.fields import RichTextUploadingField
+from shop.abstract import BaseModel, ActiveModel, OrderModel
+
+UserModel = get_user_model()
+
